2334e77a46589ddbdadd87180e6a96d058772516
Yongjin081002 <yg4059540@gmail.com>
2024-11-27 11:50:57 +0900

    feat: App.vue 경로 가져오기

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import Sidebar from './components/Sidebar.vue'
-
-const count = ref(0);
-const increment = () =>{
-    count.value += 1; 
-}
-const decrement = () => {
-  count.value -= 1;
-}
+  import Ref from './components/Ref.vue';
+  import Shopping from './components/Shopping.vue';
+  import Vif from './components/Vif.vue';
+  import Vfor from './components/Vfor.vue';
 </script>
 
-  <template >
-    <div class="mom">
-      <p>{{count}}</p>
-      <button class="countBtn" @click="increment">+</button>
-      <button class="countBtn" @click="decrement">-</button>
+<template>
+    <div>
+      <Ref />
     </div>
-  </template>
-
-  <style>
-    html,body{
-      margin:0;
-    }
-    .mom {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      height: 100vh;
-      align-items  : center;
-    }
-    .countBtn{
-      width:200px;
-      height:50px;
-      border-radius: 20px;
-    }
-    
-  </style>
+</template>
--- src/components/ref.vue
+++ src/components/ref.vue
@@ -10,8 +10,29 @@ const decrement = () => {
 }
 </script>
 
-  <template>
-    <p>{{count}}</p>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+  <template >
+    <div class="mom">
+      <p>{{count}}</p>
+      <button class="countBtn" @click="increment">+</button>
+      <button class="countBtn" @click="decrement">-</button>
+    </div>
   </template>
+
+  <style>
+    html,body{
+      margin:0;
+    }
+    .mom {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100vh;
+      align-items  : center;
+    }
+    .countBtn{
+      width:200px;
+      height:50px;
+      border-radius: 20px;
+    }
+    
+  </style>
